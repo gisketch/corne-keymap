c5c9baf52c2dbeb738caecba96563dcac2bf94b5
gisketch <dev@gisketch.com>
2025-10-28 03:59:49 +0800

    update keymappings

--- keymap.c
+++ keymap.c
@@ -24,7 +24,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 enum custom_keycodes {
     RGB_TOG_CUSTOM = SAFE_RANGE,
     GAMING_MODE,
-    DEFAULT_MODE
+    DEFAULT_MODE,
+    ARROW_R,
+    ARROW_L                // ->
 };
 
 // ============================================================================
@@ -153,31 +155,39 @@ uint8_t get_led_from_matrix(uint8_t row, uint8_t col) {
 // ============================================================================
 // KEYMAP DEFINITION
 // ============================================================================
+//
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Layer 0: Colemak-DH (Default)
     [0] = LAYOUT_split_3x6_3(
        KC_TAB,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                         KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN,  KC_DEL,
-      KC_LSFT, KC_A,  KC_R,    KC_S,    KC_T,    KC_G,                         KC_M,    KC_N,    KC_E,    KC_I,    KC_O,  KC_ENT,
+      MT(MOD_LSFT, KC_ESC), KC_A,  KC_R,    KC_S,    KC_T,    KC_G,                         KC_M,    KC_N,    KC_E,    KC_I,    KC_O,  KC_ENT,
       KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                         KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH, KC_QUOT,
-                                          KC_LALT,   MO(1),  KC_SPC,    KC_BSPC,   MO(2), KC_ESC
+                                          KC_LALT,   MO(1),  KC_SPC,    KC_BSPC,   MO(2), KC_LSFT
     ),
 
-    // Layer 1: Numbers & Symbols (Colemak)
+    // // Layer 1: Numbers & Symbols (Colemak)
+    // [1] = LAYOUT_split_3x6_3(
+    //    KC_TAB, KC_EXLM,   KC_AT, KC_LPRN, KC_RPRN, KC_BSLS,                      KC_QUOT,    KC_7,    KC_8,    KC_9, KC_ASTR,  KC_GRV,
+    //   MT(MOD_LSFT, KC_ESC), KC_HASH,  KC_DLR, KC_LCBR, KC_RCBR,  KC_GRV,                      KC_SCLN,    KC_4,    KC_5,    KC_6,  KC_EQL, KC_MINS,
+    //   KC_LCTL, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD,                      KC_AMPR,    KC_1,    KC_2,    KC_3, KC_SLSH, KC_QUOT,
+    //                                       KC_LALT, _______,  KC_SPC,    KC_BSPC,   MO(3),    KC_0
+    // ),
+    //
     [1] = LAYOUT_split_3x6_3(
-       KC_TAB, KC_EXLM,   KC_AT, KC_LPRN, KC_RPRN, KC_PIPE,                      KC_QUOT,    KC_7,    KC_8,    KC_9, KC_ASTR,  KC_GRV,
-      KC_LSFT, KC_HASH,  KC_DLR, KC_LCBR, KC_RCBR,  KC_GRV,                      XXXXXXX,    KC_4,    KC_5,    KC_6,  KC_EQL, KC_MINS,
-      KC_LCTL, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD,                      KC_AMPR,    KC_1,    KC_2,    KC_3, KC_SLSH, KC_QUOT,
+       KC_TAB, KC_EXLM,   KC_AT, KC_LPRN, KC_RPRN, KC_PIPE,                      KC_COLN,    KC_7,    KC_8,    KC_9, KC_PLUS,  KC_GRV,
+      LSFT_T(KC_ESC), KC_HASH,  KC_DLR, KC_LCBR, KC_RCBR, ARROW_R,                      KC_SCLN,    KC_4,    KC_5,    KC_6,  KC_EQL, KC_UNDS,
+      KC_LCTL, KC_PERC, KC_AMPR, KC_LBRC, KC_RBRC, ARROW_L,                      KC_QUOT,    KC_1,    KC_2,    KC_3, KC_MINS, KC_BSLS,
                                           KC_LALT, _______,  KC_SPC,    KC_BSPC,   MO(3),    KC_0
-    ),
+),
 
     // Layer 2: Functions & Navigation (Colemak)
     [2] = LAYOUT_split_3x6_3(
        KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_PGUP,
       KC_LSFT, XXXXXXX,   KC_F5,   KC_F6,   KC_F7,   KC_F8,                      KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, RGB_VAD, RGB_VAI,
       KC_LCTL, XXXXXXX,   KC_F9,  KC_F10,  KC_F11,  KC_F12,                      KC_HOME,   KC_F1,   KC_F2,   KC_F3,   KC_F4, KC_PGDN,
-                                          KC_LALT,   MO(3),  KC_SPC,    KC_BSPC, _______, KC_ESC
+                                          KC_LALT,   MO(3),  KC_SPC,    KC_BSPC, _______, KC_LSFT
     ),
 
     // Layer 3: Settings (accessible from both modes)
@@ -219,18 +229,26 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 }
             }
             return false;
-
         case GAMING_MODE:
             if (record->event.pressed) {
                 // Switch to gaming layer permanently
                 layer_move(4);
             }
             return false;
-
         case DEFAULT_MODE:
             if (record->event.pressed) {
                 // Switch back to default Colemak layer
                 layer_move(0);
+            }
+            return false;
+        case ARROW_R:
+            if (record->event.pressed) {
+                SEND_STRING("=>");
+            }
+            return false;
+        case ARROW_L:
+            if (record->event.pressed) {
+                SEND_STRING("->");
             }
             return false;
     }
